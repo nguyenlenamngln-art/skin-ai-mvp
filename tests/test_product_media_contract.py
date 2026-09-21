302f94dad84241e3ac36b469ebc1dc70314f7839
nguyenlenamngln-art <nguyenlenam.ngln@gmail.com>
2026-09-21 09:34:06 +0700

    Cover RGB V1.1 media contract

--- tests/test_product_media_contract.py
+++ tests/test_product_media_contract.py
@@ -1,7 +1,7 @@
 from skin_ai.product_api import public_scan
 
 
-def test_public_scan_exposes_all_analysis_media():
+def test_public_scan_exposes_all_uv_analysis_media():
     scan = {
         "id": "abc123",
         "created_at": "2026-09-18T00:00:00+00:00",
@@ -20,4 +20,25 @@ def test_public_scan_exposes_all_analysis_media():
         "porphyrin_mask": "/media/abc123/porphyrin_mask.png",
         "dark_mask": "/media/abc123/dark_artifact_mask.png",
         "light_mask": "/media/abc123/light_artifact_mask.png",
+    }
+
+
+def test_public_scan_exposes_rgb_v11_skin_region_media():
+    scan = {
+        "id": "rgb123",
+        "created_at": "2026-09-21T00:00:00+00:00",
+        "modality": "rgb",
+        "source_name": "face.jpg",
+        "metrics": {},
+        "media_dir": "unused",
+    }
+
+    out = public_scan(scan)
+
+    assert out["media"] == {
+        "original": "/media/rgb123/original.jpg",
+        "skin_region": "/media/rgb123/skin_region.png",
+        "overlay": "/media/rgb123/rgb_overlay.png",
+        "redness_map": "/media/rgb123/redness_map.png",
+        "pigmentation_map": "/media/rgb123/pigmentation_map.png",
     }
