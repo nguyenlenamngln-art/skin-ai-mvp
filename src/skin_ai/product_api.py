993e9377be3961a5e84b1df14efb1e8b203c1f50
nguyenlenamngln-art <nguyenlenam.ngln@gmail.com>
2026-09-23 11:17:49 +0700

    Use RGB V1.5.2 as core engine

--- src/skin_ai/product_api.py
+++ src/skin_ai/product_api.py
@@ -12,7 +12,7 @@ from skin_ai.store import ProductStore
 from skin_ai.uv_engine import UVAnalysisEngine
 from skin_ai.uv_input_validator import UVInputValidator
 from skin_ai.uv_longitudinal import checkpoint_signature, stamp_uv_longitudinal_metadata
-from skin_ai.rgb_engine_v15 import RGBAnalysisEngine
+from skin_ai.rgb_engine_v152 import RGBAnalysisEngine
 from skin_ai.tracking import get_region, region_options, stamp_tracking_metadata
 from skin_ai.session_plan import SESSION_VERSION, next_region, session_plan, session_progress
 
@@ -279,19 +279,18 @@ async def analyze_rgb(
 
 @app.post('/v1/rgb/capture-check')
 async def rgb_capture_check(image:UploadFile=File(...)):
-    """Low-resolution, non-persistent face-size check for live guidance."""
+    """Low-resolution, non-persistent face-size check for live RGB guidance."""
     rgb=decode_image(await image.read())
     engine=get_rgb_engine()
     face=engine._detect_largest_face(engine.face_detector,rgb)
     if face is None:
         return {'face_detected':False,'distance_state':'no_face','distance_label':'Center face','face_area_fraction':0.0,'capture_protocol_version':RGBAnalysisEngine.CAPTURE_PROTOCOL_VERSION}
-    h,w=rgb.shape[:2]; x,y,fw,fh=face
+    h,w=rgb.shape[:2]; _,_,fw,fh=face
     area=float((fw*fh)/max(1,h*w))
     if area<0.18: state,label='move_closer','Move closer'
     elif area>0.60: state,label='move_back','Move back'
     else: state,label='good','Distance good'
     return {'face_detected':True,'distance_state':state,'distance_label':label,'face_area_fraction':round(area,4),'capture_protocol_version':RGBAnalysisEngine.CAPTURE_PROTOCOL_VERSION}
 
-if WEB_DIR.exists():
-    app.mount('/app',StaticFiles(directory=WEB_DIR),name='app-assets')
-    app.mount('/',StaticFiles(directory=WEB_DIR,html=True),name='web')
+app.mount('/app',StaticFiles(directory=WEB_DIR,html=False),name='app')
+app.mount('/',StaticFiles(directory=WEB_DIR,html=True),name='web')
